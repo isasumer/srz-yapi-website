c525cf69679b1e17b8793e50f1f6ea1df2afa1ec
İsa Sümer <sumerisa5@gmail.com>
2025-02-01 13:04:03 +0300

    Add Google Tag Manager integration for improved tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
+import Script from 'next/script' // Import Next.js Script for optimized loading
 import './globals.css'
 
 const geistSans = localFont({
@@ -16,13 +17,13 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: 'SRZ Yapı | Cam Balkon ve Kış Bahçesi Sistemleri',
   description:
-  'SRZ cam yapı sektöründe cam balkon ve kış bahçesi sistemleri ile hizmet vermektedir. Güvenilir çözümler ve projeler için bizi arayın.',
+    'SRZ cam yapı sektöründe cam balkon ve kış bahçesi sistemleri ile hizmet vermektedir. Güvenilir çözümler ve projeler için bizi arayın.',
   keywords: 'cam balkon, kış bahçesi, SRZ cam, güvenilir çözümler, projeler',
-  authors: [{ name: 'Ramazan Çakırdım ', url: 'https://www.srzyapi.com.tr' }],
+  authors: [{ name: 'Ramazan Çakırdım', url: 'https://www.srzyapi.com.tr' }],
   openGraph: {
     title: 'SRZ cam | Güvenilir Çözümler ve Projeler',
     description:
-    'SRZ cam yapı sektöründe cam balkon ve kış bahçesi sistemleri ile hizmet vermektedir.Cam balkon ve kış bahçesi sistemleri için bizi arayın.',
+      'SRZ cam yapı sektöründe cam balkon ve kış bahçesi sistemleri ile hizmet vermektedir.Cam balkon ve kış bahçesi sistemleri için bizi arayın.',
     url: 'https://www.srzyapi.com.tr',
     siteName: 'SRZ Yapı',
     images: [
@@ -49,10 +50,31 @@ export default function RootLayout ({
       <head>
         <meta charSet='UTF-8' />
         <meta name='robots' content='index, follow' />
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PJ5WDXVR');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PJ5WDXVR"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager */}
+        
         <main>{children}</main>
       </body>
     </html>
